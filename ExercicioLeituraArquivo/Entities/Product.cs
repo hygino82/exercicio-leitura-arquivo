405712e5330857c87a9038fbcd53a88e956f3532
Adroaldo <hygino@msn.com>
2021-03-01 09:13:22 -0300

    Testando a leitura e escrita de arquivo

---
+++ ExercicioLeituraArquivo/Entities/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioLeituraArquivo.Entities
+{
+    class Product
+    {
+        public String Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product()
+        {
+        }
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double TotalPrice()
+        {
+            return Price * Quantity;
+        }
+        public string TotalItem()
+        {
+            return Name + "," + TotalPrice().ToString("F2", CultureInfo.InvariantCulture);
+        }
+        public override string ToString()
+        {
+            return Name + ", " + Price.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantity;
+        }
+    }
+}
---
+++ ExercicioLeituraArquivo/Program.cs
@@ -0,0 +1,51 @@
+﻿using ExercicioLeituraArquivo.Entities;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Collections.Generic;
+
+namespace ExercicioLeituraArquivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"c:\temp\lista.txt";
+            string targetPath = @"c:\temp\out\file2.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+                List<Product> lista = new List<Product>();
+                foreach (string linha in lines)//leitura das linhas
+                {
+                    string[] divide = linha.Split(',');
+                    string name = divide[0];
+                    double price = double.Parse(divide[1], CultureInfo.InvariantCulture);
+                    int quantity = int.Parse(divide[2]);
+
+                    Product prod = new Product(name, price, quantity);
+                    lista.Add(prod);
+                }
+                if (!Directory.Exists(targetPath))//caso não exista o diretório
+                {
+                    Directory.CreateDirectory(@"c:\temp\out");
+                }
+                //escrever os elementos da lista no arquivo
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+
+                    foreach (Product p in lista)
+                    {
+                        sw.WriteLine(p.TotalItem());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}

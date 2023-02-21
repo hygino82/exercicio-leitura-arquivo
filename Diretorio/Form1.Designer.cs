396581f4f15178db928c83c1b41fe6ce256230e8
Adroaldo <hygino@msn.com>
2023-02-21 10:51:05 -0300

    Classe Directory

---
+++ Diretorio/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Diretorio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lista = new System.Windows.Forms.ListBox();
+            this.btnExecutar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lista
+            // 
+            this.lista.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lista.FormattingEnabled = true;
+            this.lista.ItemHeight = 29;
+            this.lista.Location = new System.Drawing.Point(13, 13);
+            this.lista.Name = "lista";
+            this.lista.Size = new System.Drawing.Size(701, 323);
+            this.lista.TabIndex = 0;
+            // 
+            // btnExecutar
+            // 
+            this.btnExecutar.BackColor = System.Drawing.Color.PaleGreen;
+            this.btnExecutar.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExecutar.Location = new System.Drawing.Point(278, 373);
+            this.btnExecutar.Name = "btnExecutar";
+            this.btnExecutar.Size = new System.Drawing.Size(183, 50);
+            this.btnExecutar.TabIndex = 1;
+            this.btnExecutar.Text = "Executar";
+            this.btnExecutar.UseVisualStyleBackColor = false;
+            this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 435);
+            this.Controls.Add(this.btnExecutar);
+            this.Controls.Add(this.lista);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lista;
+        private System.Windows.Forms.Button btnExecutar;
+    }
+}
+
---
+++ Diretorio/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Diretorio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+
+            string path = @"C:\curso\";
+            string folder = "dilma";
+
+            bool res = Directory.Exists(path + folder);
+            //lista.Items.Add(res);
+
+            //if (!res)
+            //{
+            //    Directory.CreateDirectory(path + folder);
+            //    lista.Items.Add("Criada a pasta " + folder);
+            //}
+
+            //if(res)
+            //{
+            //    Directory.Delete(path + folder);
+            //    lista.Items.Add("Removida a pasta " + folder);
+            //}
+
+            //if (res)
+            //{
+            //    Directory.Move(path + folder, path + "destino\\dilmaCopia");
+            //    lista.Items.Add("Movido para a pasta destino");
+            //}
+
+
+            //string[] pastas = Directory.GetDirectories(path);
+            //foreach (string pasta in pastas)
+            //{
+            //    lista.Items.Add(pasta);
+            //}
+
+            //string[] arquivos = Directory.GetFiles(path);
+            //foreach (string arquivo in arquivos)
+            //{
+            //    lista.Items.Add(arquivo);
+            //}
+
+            //lista.Items.Add(Directory.GetDirectoryRoot(path));
+
+            //string[] drives = Directory.GetLogicalDrives();
+            //lista.Items.AddRange(drives);
+
+            //var info = Directory.GetParent(path);
+            //lista.Items.Add(info.FullName);
+
+            var caminho = Directory.GetCurrentDirectory();
+            //diretório aonde a aplicação é executada
+            lista.Items.Add(caminho);
+        }
+    }
+}
+
+

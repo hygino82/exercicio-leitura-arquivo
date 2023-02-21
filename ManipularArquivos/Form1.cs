0763e683ba7a124ad3edfafc01d1bea1a8bbcd56
Adroaldo <hygino@msn.com>
2023-02-21 09:58:54 -0300

    Classe FileInfo

---
+++ ManipularArquivos/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManipularArquivos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExecutar_Click(object sender, EventArgs e)
+        {
+            lista.Items.Clear();
+            FileInfo info = new FileInfo(@"c:\curso\teste.txt");
+            if (info.Exists)
+            {
+                lista.Items.Add(info.FullName);//caminho completo do arquivo
+                lista.Items.Add(info.Name);//nome do arquivo
+                lista.Items.Add(info.Extension);
+                lista.Items.Add(info.Directory.FullName);
+                lista.Items.Add(info.DirectoryName);
+                lista.Items.Add(info.CreationTime);
+                lista.Items.Add(info.CreationTimeUtc);
+                lista.Items.Add(info.Length);
+            }
+            else
+            {
+                lista.Items.Add("O Arquivo " + info.Name + " não existe!");
+            }
+
+        }
+    }
+}
